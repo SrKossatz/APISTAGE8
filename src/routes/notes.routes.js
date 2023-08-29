93e5fe731168fe3f172cbcd65e8edbe4c36f16ff
Kossatz <atyllakossatz@gmail.com>
2023-08-29 19:35:03 +0100

    delete notes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express"); // import da Router de dentro do express
 const notesRoutes = Router() // inicializa o Router
 
-const NotesController = require("../controllers/NotesController") //import da classe
+const NotesController = require("../controllers/notesController") //import da classe
 const notesController = new NotesController()//instânciar a classe 
 
 
@@ -13,6 +13,8 @@ const notesController = new NotesController()//instânciar a classe
 
 //todas as vezes que a rota userRouters for chamada eu quero usar o meu controller
 notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.get("/:id", notesController.show);
+notesRoutes.delete("/:id", notesController.delete);
 
 
 module.exports = notesRoutes; // código para exportar a rota do usersRoutes. Arquivo fica disponível para ser reutilizado no server.js por exemplo.
